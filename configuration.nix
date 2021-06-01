09c694a8e60c5f40784995b3f56f9037184c4c8f
Vanilla <neko@hydev.org>
2021-06-01 11:19:08 +0000

    [-] Comment wifi device by Xiaomi portable Wifi.

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   networking.useDHCP = false;
   networking.interfaces.enp3s0f1.useDHCP = true;
   networking.interfaces.wlp0s20f0u2u1u2.useDHCP = true;
-  networking.interfaces.wlp0s20f3.useDHCP = true;
+  # networking.interfaces.wlp0s20f3.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
