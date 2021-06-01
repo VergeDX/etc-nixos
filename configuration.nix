4fe34e0687101f4857ae68e1bc2e056999fc014b
Vanilla <neko@hydev.org>
2021-06-01 11:23:29 +0000

    [+] Add normal user vanilla & enable sudo.

--- configuration.nix
+++ configuration.nix
@@ -64,10 +64,10 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
+  users.users.vanilla = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
