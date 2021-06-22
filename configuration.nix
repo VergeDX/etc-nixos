b50a7d8141f6b0207da6fd8d253f222d6324a71c
Vanilla <neko@hydev.org>
2021-06-22 19:01:40 +0800

    [+] Enable PipeWire audio.

--- configuration.nix
+++ configuration.nix
@@ -72,7 +72,17 @@
 
   # Enable sound.
   # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  # https://nixos.wiki/wiki/PipeWire
+  # https://blog.ryey.icu/zhs/replace-pulseaudio-with-pipewire.html
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
