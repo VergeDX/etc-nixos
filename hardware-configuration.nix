24ef28644f986caf9a29f18d0175d95e99647036
Vanilla <neko@hydev.org>
2021-07-26 20:29:47 +0800

    [+] Override bluetooth unit for disable load sap plugin.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -36,6 +36,13 @@
   services.blueman.enable = true;
   hardware.enableAllFirmware = true;
 
+  # https://raspberrypi.stackexchange.com/questions/40839/sap-error-on-bluetooth-service-status
+  # https://github.com/NixOS/nixpkgs/issues/63703
+  systemd.services.bluetooth.serviceConfig.ExecStart = [
+    ""
+    "${pkgs.bluez}/libexec/bluetooth/bluetoothd --noplugin=sap"
+  ];
+
   nixpkgs.config.allowUnfree = true;
   # https://nixos.wiki/wiki/Nvidia
   services.xserver.videoDrivers = [ "nvidia" ];
