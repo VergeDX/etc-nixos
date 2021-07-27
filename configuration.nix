880a338277caa1cfa8c71d732a16a3c71e78a4d4
Vanilla <neko@hydev.org>
2021-07-27 19:56:57 +0800

    [M] Changes kernel to linux-lts, try to fix network stability.

--- configuration.nix
+++ configuration.nix
@@ -146,9 +146,10 @@ in
   nix.extraOptions = "experimental-features = nix-command flakes";
 
   # https://nixos.wiki/wiki/Linux_kernel
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  # boot.kernelPackages = pkgs.linuxPackages_zen;
   # https://gist.github.com/manuelmazzuola/4ffa90f5f5d0ddacda96#file-configuration-nix-L22
   boot.kernel.sysctl = { "kernel.sysrq" = 1; };
+  boot.kernelParams = [ "pcie_aspm.policy=performance" ];
 
   # i18n.inputMethod.enabled = "ibus";
   # i18n.inputMethod.ibus.engines = [ pkgs.ibus-engines.rime ];
