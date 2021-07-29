4a0bad7c719f042bb7567a6bb4db24e6bb5a7d70
Vanilla <neko@hydev.org>
2021-07-29 09:03:26 +0800

    [+] Firewall: disable ping & reject packages.

--- configuration.nix
+++ configuration.nix
@@ -133,6 +133,9 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  networking.firewall.allowPing = false;
+  networking.firewall.rejectPackets = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
