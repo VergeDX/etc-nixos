d00714042869c01bfc2be3cbd70896d833540b18
Vanilla <neko@hydev.org>
2021-08-06 19:27:10 +0800

    [+] Add zstd compress & noatime for btrfs.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -18,6 +18,11 @@
     {
       device = "/dev/disk/by-uuid/4ff541a0-7952-4ede-a20b-6cecba4f16eb";
       fsType = "btrfs";
+
+      # https://btrfs.wiki.kernel.org/index.php/Compression
+      # https://wiki.archlinux.org/title/Btrfs_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)#%E5%8E%8B%E7%BC%A9
+      # https://btrfs.wiki.kernel.org/index.php/FAQ#Performance_vs_Correctness
+      options = [ "compress-force=zstd" "noatime" ];
     };
 
   fileSystems."/boot" =
