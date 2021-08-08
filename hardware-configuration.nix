389e8c789411ee098abec38c34c6e3d74f3c0471
Vanilla <neko@hydev.org>
2021-08-08 20:16:27 +0800

    [+] Config my yubikeys, using password + key mode.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -73,4 +73,10 @@
   # https://github.com/NixOS/nixpkgs/issues/101281
   hardware.xpadneo.enable = true;
   services.hardware.xow.enable = true;
+
+  # https://nixos.wiki/wiki/Yubikey
+  security.pam.yubico.enable = true;
+  # security.pam.yubico.debug = true;
+  security.pam.yubico.mode = "challenge-response";
+  security.pam.yubico.control = "required";
 }
