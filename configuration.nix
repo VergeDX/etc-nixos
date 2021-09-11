f082676ad5911db20c63749f769e34e3314f6b27
Vanilla <neko@hydev.org>
2021-09-11 19:20:41 +0800

    [-] configuration.nix: Remove useless comment of proxy.

--- configuration.nix
+++ configuration.nix
@@ -44,7 +44,6 @@ in
   networking.dhcpcd.enable = false;
 
   # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
   networking.proxy.default = "http://localhost:8889";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
