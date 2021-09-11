f77ed35b5c9d7afdf4b9664cab71e72476a39dde
Vanilla <neko@hydev.org>
2021-09-11 20:46:17 +0800

    [-] Remove unused comment of the two .nix file.

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
-
 let
   # https://userbase.kde.org/KDEConnect
   kde-connect-port-range = { from = 1714; to = 1764; };
