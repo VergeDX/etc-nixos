976be93c810c3519a024794ec228a99dbecaaf3f
Vanilla <neko@hydev.org>
2021-09-11 20:43:52 +0800

    [M] hardware-configuration.nix -> hardware.nix.

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@ in
   imports =
     [
       # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
