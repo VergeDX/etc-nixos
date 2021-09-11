5e3329b89b880b6f881e54f7f8db254e66719528
Vanilla <neko@hydev.org>
2021-09-11 22:24:48 +0800

    [M] Split config to single file: hardware/disks.nix.

--- hardware/hardware.nix
+++ hardware/hardware.nix
@@ -1,25 +1,15 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+
+    ./disks.nix
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   # boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/4ff541a0-7952-4ede-a20b-6cecba4f16eb";
-      fsType = "btrfs";
-
-      # https://btrfs.wiki.kernel.org/index.php/Compression
-      # https://wiki.archlinux.org/title/Btrfs_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)#%E5%8E%8B%E7%BC%A9
-      # https://btrfs.wiki.kernel.org/index.php/FAQ#Performance_vs_Correctness
-      options = [ "compress-force=zstd" "noatime" ];
-    };
-
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/F934-BA50"; fsType = "vfat"; };
-  swapDevices = [{ device = "/dev/disk/by-uuid/1b07ef58-fa1b-4e5e-a394-7a07a9229f07"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
