076a1d475fe188247b46ea685bc3d32a577f11fb
Vanilla <neko@hydev.org>
2021-09-11 22:28:36 +0800

    [M] Split config to single file: hardware/bluetooth.nix.

--- hardware/hardware.nix
+++ hardware/hardware.nix
@@ -5,24 +5,13 @@
 
     ./disks.nix
     ./cpu.nix
+    ./bluetooth.nix
   ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   # boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  # https://nixos.wiki/wiki/Bluetooth
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-  hardware.enableAllFirmware = true;
-
-  # https://raspberrypi.stackexchange.com/questions/40839/sap-error-on-bluetooth-service-status
-  # https://github.com/NixOS/nixpkgs/issues/63703
-  systemd.services.bluetooth.serviceConfig.ExecStart = [
-    ""
-    "${pkgs.bluez}/libexec/bluetooth/bluetoothd --noplugin=sap"
-  ];
 
   nixpkgs.config.allowUnfree = true;
   # https://nixos.wiki/wiki/Nvidia
