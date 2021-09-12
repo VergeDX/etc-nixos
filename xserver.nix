252fd2e37933d5312dd498784cea75f88d3ba48a
Vanilla <neko@hydev.org>
2021-09-12 22:17:52 +0800

    [+] xserver.nix: Enable nvidia wayland.

--- xserver.nix
+++ xserver.nix
@@ -5,6 +5,8 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.nvidiaWayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
