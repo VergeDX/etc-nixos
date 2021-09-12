ad718f6daf95ad4c50822f4173d05b1a09cd98da
Vanilla <neko@hydev.org>
2021-09-12 20:55:31 +0800

    [+] hardware/nvidia.nix: Command of nvidia-offload.

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,5 +1,15 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+let nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+  export __NV_PRIME_RENDER_OFFLOAD=1
+  export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+  export __GLX_VENDOR_LIBRARY_NAME=nvidia
+  export __VK_LAYER_NV_optimus=NVIDIA_only
+  exec -a "$0" "$@"
+'';
+in
 {
+  # https://nixos.wiki/wiki/Nvidia#offload_mode
+  environment.systemPackages = [ nvidia-offload ];
   nixpkgs.config.allowUnfree = true;
 
   # https://github.com/NickCao/flakes/blob/baaa99e3b32ca01069443aa0466c6aeefe3620a4/nixos/local/configuration.nix#L104
