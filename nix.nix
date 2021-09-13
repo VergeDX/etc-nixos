3e26d6d30620db3cc83031a8ef5f9b07fd812f6b
Vanilla <neko@hydev.org>
2021-09-13 12:06:33 +0800

    [M] nix.nix: nixFlakes -> nixUnstable.

--- nix.nix
+++ nix.nix
@@ -1,12 +1,8 @@
 { pkgs, lib, config, ... }:
 {
   # https://nixos.wiki/wiki/Flakes
-  # https://discourse.nixos.org/t/using-experimental-nix-features-in-nixos-and-when-they-will-land-in-stable/7401/3
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
-      "experimental-features = nix-command flakes";
-  };
+  nix.package = pkgs.nixUnstable;
+  nix.extraOptions = "experimental-features = nix-command flakes";
 
   # https://nixos.org/manual/nixpkgs/stable/#submitting-changes-tested-with-sandbox
   nix.useSandbox = true;
