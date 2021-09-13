0aabe4dafe594a734f53d0cc8a9ae9d9f377b85f
Vanilla <neko@hydev.org>
2021-09-13 11:10:52 +0800

    [+] i18n.nix: Sync Arch's build options to enable cloud pinyin. '

--- i18n.nix
+++ i18n.nix
@@ -5,7 +5,15 @@
   console.keyMap = "us";
 
   i18n.inputMethod.enabled = "ibus";
-  i18n.inputMethod.ibus.engines = [ pkgs.ibus-engines.libpinyin ];
+  i18n.inputMethod.ibus.engines = [
+    (pkgs.ibus-engines.libpinyin.overrideAttrs (old: {
+      # https://github.com/archlinux/svntogit-community/blob/packages/ibus-libpinyin/trunk/PKGBUILD
+      configurePhase = "./autogen.sh --prefix=$out --enable-boost --enable-opencc --enable-cloud-input-mode";
+      prePatch = ''substituteInPlace autogen.sh --replace "which" "${pkgs.which}/bin/which"'';
+      buildInputs = with pkgs; old.buildInputs ++ [ gnome.gnome-common boost opencc libsoup json-glib lua ];
+    }))
+  ];
+
   # i18n.inputMethod.enabled = "fcitx5";
   # i18n.inputMethod.fcitx5.addons = [ pkgs.fcitx5-chinese-addons ];
 }
