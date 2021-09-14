e4ad599f549c996784cbbb2090f293aef4a24ddd
Vanilla <neko@hydev.org>
2021-09-14 11:30:51 +0800

    [+] i18n.nix: --libexecdir for ibus libpinyin, from ArchLinux.

--- i18n.nix
+++ i18n.nix
@@ -8,7 +8,7 @@
   i18n.inputMethod.ibus.engines = [
     (pkgs.ibus-engines.libpinyin.overrideAttrs (old: {
       # https://github.com/archlinux/svntogit-community/blob/packages/ibus-libpinyin/trunk/PKGBUILD
-      configurePhase = "./autogen.sh --prefix=$out --enable-boost --enable-opencc --enable-cloud-input-mode";
+      configurePhase = "./autogen.sh --prefix=$out --libexecdir=$out/lib/${old.pname} --enable-boost --enable-opencc --enable-cloud-input-mode";
       prePatch = ''substituteInPlace autogen.sh --replace "which" "${pkgs.which}/bin/which"'';
       buildInputs = with pkgs; old.buildInputs ++ [ gnome.gnome-common boost opencc libsoup json-glib lua ];
     }))
