71b7464b6bbef94cf8aa2cfd5c9be42de646e3de
Vanilla <neko@hydev.org>
2021-09-24 15:35:26 +0800

    [-] services.nix: Remove printing service.

--- services.nix
+++ services.nix
@@ -1,11 +1,5 @@
 { ... }:
 {
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  # https://nixos.wiki/wiki/Printing
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.passwordAuthentication = false;
