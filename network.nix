e2f30129dcc37430e8aeb4c6aae52a24e937455d
Vanilla <neko@hydev.org>
2021-09-26 18:28:59 +0800

    [+] network.nix: Add proxy.noProxy config.

--- network.nix
+++ network.nix
@@ -22,7 +22,7 @@
 
   # Configure network proxy if necessary
   networking.proxy.default = "http://localhost:7890";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 8080 7890 ];
