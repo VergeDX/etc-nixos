1cc30afa38e94018f639cc3bb1ef468abddbc39c
Vanilla <osu_Vanilla@126.com>
2021-09-28 17:37:46 +0800

    [+] hardware/yubikey.nix: Enable GPG and SSH.

--- hardware/yubikey.nix
+++ hardware/yubikey.nix
@@ -2,6 +2,16 @@
 {
   # https://nixos.wiki/wiki/Yubikey
   security.pam.yubico.enable = true;
+
+  # https://nixos.wiki/wiki/Yubikey#GPG_and_SSH
+  programs.ssh.startAgent = false;
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  environment.shellInit = ''
+    export GPG_TTY="$(tty)"
+    gpg-connect-agent /bye
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+  '';
+
   # security.pam.yubico.debug = true;
   security.pam.yubico.mode = "challenge-response";
   security.pam.yubico.control = "required";
