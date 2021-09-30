38ea7bec39795d66d8ddb087ea3908f04e6ef8ef
Vanilla <osu_Vanilla@126.com>
2021-09-30 09:06:34 +0800

    [+] services.nix: Pass self signed cert patch.

--- services.nix
+++ services.nix
@@ -39,9 +39,22 @@
       "tls-key" = "${CREDENTIALS_DIRECTORY}/key";
     };
 
-  systemd.services."telegraf".serviceConfig = {
-    ExecStart = lib.mkForce "${config.services.telegraf.package}/bin/telegraf --config $config";
-  };
+  systemd.services."telegraf".serviceConfig."ExecStart" =
+    let
+      dir = "/tmp/telegraf/";
+      script = pkgs.writeText "isv.sh" ''
+        # https://docs.influxdata.com/influxdb/v1.8/administration/https_setup/#connect-telegraf-to-a-secured-influxdb-instance
+        mkdir -p ${dir} && rm ${dir}/telegraf.conf && true
+
+        # https://stackoverflow.com/questions/65768636/influxdb-reports-unauthorized-access-with-the-generated-token
+        ${pkgs.curl}/bin/curl $config -k --header "Authorization: Token $INFLUX_TOKEN" > ${dir}/telegraf.conf && chmod o-r ${dir}/telegraf.conf
+
+        # https://stackoverflow.com/questions/6537490/insert-a-line-at-specific-line-number-with-sed-or-awk/6537587
+        ${pkgs.gnused}/bin/sed -i '65i  insecure_skip_verify = true' ${dir}/telegraf.conf
+        ${config.services.telegraf.package}/bin/telegraf --config ${dir}/telegraf.conf
+      '';
+    in
+    lib.mkForce "${pkgs.bash}/bin/bash ${script}";
 
   environment.systemPackages = with pkgs; [ xmonad-with-packages xterm ];
   services.xrdp.enable = true;
